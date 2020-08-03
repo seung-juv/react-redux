d663722b7c441a036542a695bf78e3f4e6ecbb48
seungju <kingsky32@gmail.com>
2020-08-03 13:51:31 +0900

    Detail Screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ text, id, onBtnClick }) => {
   return (
     <li>
-      {text}
+      <Link to={`/${id}`}>
+        {text}
+      </Link>
       <button onClick={onBtnClick}>DEL</button>
     </li>
   );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+const Detail = ({ toDo }) => {
+  return (
+    <div>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const { match: { params: { id } } } = ownProps;
+  return {
+    toDo: state.find(toDo => toDo.id === parseInt(id))
+  };
+};
+
+export default connect(mapStateToProps)(Detail);
